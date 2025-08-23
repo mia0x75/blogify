398465e9e3f598d5c549d954ac468af8cc2dff07
Rajdeep Singh <radewala.2@gmail.com>
2025-08-23 23:01:47 +0530

    update the svg icons

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,36 +1,7 @@
-import React from "react";
-
 export function Logo() {
   return (
-    <svg
-      width="28"
-      height="28"
-      viewBox="0 0 500 500"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        y="500"
-        width="500"
-        height="500"
-        rx="250"
-        transform="rotate(-90 0 500)"
-        fill="#F24D15"
-      />
-      <path
-        d="M250 437C350.516 437 432 353.277 432 250C432 146.723 350.516 63 250 63C149.484 63 68 146.723 68 250C68 353.277 149.484 437 250 437Z"
-        stroke="white"
-        strokeWidth="18"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <circle
-        cx="249.5"
-        cy="249.5"
-        r="22.5"
-        transform="matrix(0 -1 1 0 0 499)"
-        fill="white"
-      />
-    </svg>
+    <div className="group h-14 rounded-2xl bg-[#7ed957] p-2">
+      <h2 className="cursor-pointer text-center text-4xl font-bold">B.</h2>
+    </div>
   );
 }
