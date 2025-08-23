dee7c0cf672b026dd407db7714cc7b200ba82ac0
Rajdeep Singh <radewala.2@gmail.com>
2025-08-23 23:03:41 +0530

    rewrite the home page ui.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import { Hero } from "@/components/Hero";
 import { GetAllPosts } from "@/data";
 import { Metadata } from "next";
 import { BlogCard } from "@/components/blog_card";
-import { Mail, Rss, X } from "lucide-react";
+import { Mail, Rss } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { TwitterIcon } from "@/components/icons/twitter";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home | Blogify",
@@ -16,7 +18,7 @@ export default function Page() {
   const posts = GetAllPosts();
   return (
     <div className="container py-24">
-      <div className="mb-16 flex flex-col items-start justify-between border-b border-border py-6 md:flex-row md:items-center">
+      <div className="mb-24 flex flex-col items-start justify-between border-b border-border py-10 md:flex-row md:items-center">
         <div className="flex flex-col gap-y-3">
           <h1 className="text-4xl font-bold text-foreground">Blog</h1>
           <p className="group flex items-center gap-1 text-sm font-medium md:text-base lg:text-lg">
@@ -27,18 +29,27 @@ export default function Page() {
           <span className="hidden text-sm text-muted-foreground lg:block">
             Subscribe via
           </span>
-          <Button variant="ghost" size="icon" className="rounded-full">
-            <Mail className="h-4 w-4" />
-          </Button>
-          <Button variant="ghost" size="icon" className="rounded-full">
-            <Rss className="h-4 w-4" />
-          </Button>
+          <Link target="_blank" href="#">
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Mail className="h-4 w-4" />
+            </Button>
+          </Link>
           <span className="hidden text-sm text-muted-foreground lg:block">
             Follow on
           </span>
-          <Button variant="ghost" size="icon" className="rounded-full">
-            <IconX />
-          </Button>
+          <Link target="_blank" href="#">
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Rss className="h-4 w-4" />
+            </Button>
+          </Link>
+          <span className="hidden text-sm text-muted-foreground lg:block">
+            and
+          </span>
+          <Link target="_blank" href="#">
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <TwitterIcon />
+            </Button>
+          </Link>
         </div>
       </div>
       <div className="mx-auto grid grid-cols-1 gap-5 lg:grid-cols-2">
@@ -48,20 +59,5 @@ export default function Page() {
       </div>
       <Newsletter />
     </div>
-  );
-}
-
-function IconX() {
-  return (
-    <svg
-      className="h-4 w-4"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      stroke="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title>X</title>
-      <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-    </svg>
   );
 }
